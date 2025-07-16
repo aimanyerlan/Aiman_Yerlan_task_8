c397bc8a5c76804b135a29f8e51149b956742790
aimanyerlan <yerlanaiman@mail.ru>
2025-07-16 15:47:13 +0500

    Полная переработка проекта

--- app/database.py
+++ app/database.py
@@ -1,17 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.orm import Session
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-SQLALCHEMY_DATABASE_URL = "postgresql://yerlanaiman:fghzxa01@localhost/dbtask3"
+from app.config import SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
